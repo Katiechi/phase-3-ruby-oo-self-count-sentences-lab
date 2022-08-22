2fe66dff75733bc28578f05341f5c5f9201db0fa
brian ambundo <brian.katiechi9@gmail.com>
2022-08-22 10:53:43 +0300

    commit tree cfcc0b49dd8e1e5a63cb8773a805d463fb80d399

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    counts = self.split(/[.?!]+/).grep(/\S/).count
+    return counts
   end
 end
+
+"The weather outside is frightful.".sentence?
+"What's your name?".question?
+"Hi, my name is Sophie!".exclamation?
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
